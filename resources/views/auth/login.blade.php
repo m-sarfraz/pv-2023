04496fddc662749492bf382cd23a6743e1ddd8f7
websolprovider <54984350+websolprovider@users.noreply.github.com>
2021-08-13 11:14:41 +0500

    html structure of suth section updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,60 @@
-@extends('layouts.app')
-
+@extends('layouts.auth-app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    <div class="loginpageS d-flex flex-wrap w-100">
+        <div class="col-lg-6 bg-white">
+            <form class="loginForm">
+                <div class="d-flex justify-content-center pb-2">
+                    <img style="width: 25px;" src="{{'assets/loginIcon.png'}}" alt="" />
                 </div>
+                <div class="form-group">
+                    <label class="mb-0 d-block font-size-3 mb-1">
+                        Email
+                    </label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                                        <span class="input-group-text EmailIcon" id="inputGroupPrepend2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"
+                                                />
+                                            </svg>
+                                        </span>
+                        </div>
+                        <input class="form-control EmailInput-F" id="email" name="email" placeholder="Enter Email Address" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="mb-1 d-block font-size-3">
+                        Password
+                    </label>
+                    <input type="password" class="form-control EmailInput-F" name="password" />
+                </div>
+                <div class="form-group">
+                    <a class="font-size-3 text-dodger line-height-reset">
+                        Forget Password
+                    </a>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button style="background-color: #dc8627;" type="submit" class="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="col-lg-6 loginLogo">
+            <div class="d-flex justify-content-center">
+                <img style="width: 249px;" src="{{ 'assets/LoginForm.png' }}" alt="" />
+            </div>
+            <div class="d-flex justify-content-center" style="padding-top: 159px;">
+                <h5 style="color: white; font-size: 28px;">
+                    Welcome to
+                </h5>
+            </div>
+            <div class="d-flex justify-content-center">
+                <h5 style="color: white; font-size: 65px;">
+                    Eallaine
+                </h5>
             </div>
         </div>
     </div>
-</div>
 @endsection
