bf0810513319039492eee689731ffe1acc6ffa51
websolprovider <54984350+websolprovider@users.noreply.github.com>
2021-08-16 12:38:04 +0500

    changes for password section as well as admin panel integration

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css')  }}" />
+    <link rel="stylesheet" href="{{ asset('assets/custom_css/login.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <div class="BoxTop"></div>
+        <div class="BoxBottom"></div>
+        <div class="width-78 m-auto vh-100 d-flex align-items-center justify-content-center">
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
