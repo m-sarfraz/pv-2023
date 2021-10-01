b99d7d15202da24db79abf50d383dbf3e4554b11
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-10-01 14:57:44 +0500

    changes

--- resources/views/profile/edit_profile.blade.php
+++ resources/views/profile/edit_profile.blade.php
@@ -61,7 +61,7 @@
                                         class="w-100 border-top-0 border-right-0 border-left-0" />
                                 </div>
                                 @can('save-profile')
-                                    <button class="costum-buttom">Submit</button>
+                                    <button class="costumButton px-3 py-1">Submit</button>
                                 @endcan
                             </form>
                         </div>
