235d94b332b3b9e8f9b6929cf8e1d937fa4e20bc
Tauqeer <abdulrehman@ewdtech.com>
2021-10-05 17:46:22 +0500

    cip added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\CandidateInformation;
 use Illuminate\Http\Request;
 use App\Charts\SampleChart;
 use Illuminate\Support\Facades\DB;
-Use App\User;
+use App\User;
+use Google\Service\AndroidEnterprise\Resource\Users;
+use Auth;
+use App\Cipprogress;
+use App\Endorsement;
+use Spatie\Permission\Models\Role;
+
 class HomeController extends Controller
 {
     /**
@@ -25,26 +32,32 @@ class HomeController extends Controller
      */
     public function index()
     {
+      
         $users = User::select(DB::raw("COUNT(*) as count"))
-        ->whereYear('created_at', date('Y'))
-        ->groupBy(DB::raw("Month(created_at)"))
-        ->pluck('count');
+            ->whereYear('created_at', date('Y'))
+            ->groupBy(DB::raw("Month(created_at)"))
+            ->pluck('count');
         $chart = new SampleChart();
         $chart->labels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
-        $chart->dataset('New User Register Chart', 'line', $users)->options([
-            'fill' => 'true',
-            'borderColor' => '#51C1C0',
-            
+        $chart->dataset('TAT ', 'bar', $users)->options([
+            // 'fill' => 'false',
+            'borderColor' => 'rgb(64, 135, 242)',
+            'backgroundColor' => 'rgb(64, 135, 242)',
+
         ]);
-        $count_user_pie=new SampleChart();
+        $chart->dataset('Profile', 'bar', [2])->options([
+            // 'fill' => 'false',
+            'borderColor' => 'rgb(253, 152, 0)',
+            'backgroundColor' => 'rgb(253, 152, 0)',
+
+        ]);
+        $count_user_pie = new SampleChart();
         $count_user_pie->labels(['First', 'Second', 'Third']);
-        $count_user_pie->dataset('my chart', 'pie', [3,3,3])->options([
+        $count_user_pie->dataset('my chart', 'pie', [3, 3, 3])->options([
             'fill' => 'true',
-            'borderColor' => '#51C1C0',
-            'background'=>"#51C1C0",
-            
-        ]);;
-        return view('home',compact("chart","count_user_pie"));
+            'borderColor' => 'yellow',
+            'backgroundColor' => 'orange',
+        ]);
+        return view('home', compact("chart", "count_user_pie"));
     }
-  
 }
