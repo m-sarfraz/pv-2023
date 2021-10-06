494bcf498bb393150d5b3d5f8a95a8c2b3ed2504
Tauqeer <abdulrehman@ewdtech.com>
2021-10-06 12:08:53 +0500

    log updated

--- app/Http/Controllers/Admin/LogController.php
+++ app/Http/Controllers/Admin/LogController.php
@@ -10,7 +10,8 @@ class LogController extends Controller
     //
     public function index()
     {
-        $logs = Log::all();
+        $logs = Log::orderBy('id', 'DESC')->get();
+       
         return view('logs.log', compact('logs'));
     }
 }
