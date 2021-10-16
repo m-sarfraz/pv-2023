05726c9e06fe03065b2fb24d91064e86bc4abef4
sarfaraz-dev <muhammadsarfraz531@gmail.com>
2021-10-16 11:59:31 +0500

    jdl work

--- app/Http/Controllers/Admin/JdlController.php
+++ app/Http/Controllers/Admin/JdlController.php
@@ -147,10 +147,8 @@ class JdlController extends Controller
             ->join("domains", "candidate_domains.domain", "domains.domain_name")
             ->whereIn("endorsements.client", $request->client)
             ->select("candidate_domains.domain", "domains.id as D_id")
-            ->groupby("candidate_domains.domain")
+            ->groupby("domains.id", 'candidate_domains.domain')
             ->get();
-
-
 
         return response()->json($filter_Client_domain);
     }
