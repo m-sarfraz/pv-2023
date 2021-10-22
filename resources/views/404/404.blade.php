f56e6c7ba632211b3c215ff1aa9d6c7ab024b155
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-10-22 11:46:15 +0500

    changes

---
+++ resources/views/404/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.auth-app')
+@section('content')
+    <div class="loginpageS d-flex flex-wrap w-100">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ad aliquid natus quibusdam eos harum fugiat officiis, sunt minus, culpa saepe. Ullam harum molestiae, est nemo distinctio modi delectus vero.
+    </div>
+@endsection
