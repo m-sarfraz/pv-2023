2bff30e4c7bf7e2bd73f3f3310ea8978280e8765
Tauqeer <abdulrehman@ewdtech.com>
2021-12-08 13:01:19 +0500

    asdfasdfasd

--- app/Http/Controllers/Admin/FinanceController.php
+++ app/Http/Controllers/Admin/FinanceController.php
@@ -251,7 +251,7 @@ class FinanceController extends Controller
     // close
 
     // append the summary of filtered record
-    public function summaryAppend(Request $request)
+    public function summaryAppend()
     {
         // $check = $searchCheck = false;
         $arr = ['Onboarded', 'Offer Accepted', 'Fallout'];
@@ -267,8 +267,8 @@ class FinanceController extends Controller
         }
         // end modify  wherein()
         if (strpos($sql, 'where') !== false) {
-            $sql_fallout = $sql . "  and endorsements.remarks LIKE '%fallout%' OR endorsements.remarks LIKE '%replacement%'  ";
-            $sql_billed = $sql . " and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%'  ";
+            $sql_fallout = $sql . "  and endorsements.remarks LIKE '%fallout%' OR endorsements.remarks LIKE '%replacement%' group by endorsements.remarks  ";
+            $sql_billed = $sql . " and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' group by endorsements.remarks  ";
             $sql_unBilled = $sql . " and endorsements.remarks ='Unbilled' ";
             $sql_billed_amount = DB::select($sql_billed);
             $sql_unbilled_amount = DB::select($sql_unBilled);
@@ -276,10 +276,10 @@ class FinanceController extends Controller
             $sql_receivables = $sql . " and finance_detail.process_status in('OVERDUE','FFUP','RCVD') ";
             $sql_Current_receivables = $sql . " and finance_detail.process_status in('FFUP','RCVD')  ";
             $sql_overDue_receivables = $sql . " and finance_detail.process_status ='OVERDUE' ";
-            $sql_billed = $sql . "  and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' ";
-            $c_share = $sql . "  and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' ";
+            $sql_billed = $sql . "  and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' group by endorsements.remarks  ";
+            $c_share = $sql . "  and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' group by endorsements.remarks  ";
             // $sql_unbilled = $sql . "  and endorsements.remarks='Unbilled'";
-            $vcc_amount_sum = $sql . " and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' ";
+            $vcc_amount_sum = $sql . " and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' group by endorsements.remarks  ";
             // $sql_onboarded = $sql . " and endorsements.remarks_for_finance='Onboarded'";
         };
 
@@ -321,15 +321,15 @@ class FinanceController extends Controller
         }
         foreach ($sql_ctake_amount as $ctake) {
             //unknown vlaue for some
-            $ctakeAmount = $ctakeAmount + $ctake->c_take_per;
+            $ctakeAmount = $ctakeAmount + $ctake->c_take;
         }
         foreach ($sql_c_share_db as $cftake) {
             //unknown vlaue for some
-            $sql_c_share = $sql_c_share + $cftake->c_take_per;
+            $sql_c_share = $sql_c_share + $cftake->c_take;
         }
         foreach ($sql_vcc_amount_sum as $cftake) {
             //unknown vlaue for some
-            $vcc_amount_sum = $vcc_amount_sum + $cftake->c_take_per;
+            $vcc_amount_sum = $vcc_amount_sum + $cftake->c_take;
         }
 
         $data = [
@@ -377,46 +377,136 @@ class FinanceController extends Controller
     // close
     public function FinanceSearchForSummary(Request $request)
     {
-        // $check = $searchCheck = false;
-        $arr = ['Onboarded', 'Offer Accepted', 'Fallout'];
-        $Userdata = Finance::join('endorsements', 'endorsements.candidate_id', 'finance.candidate_id')
-            ->join('finance_detail', 'finance_detail.candidate_id', 'finance.candidate_id')
-            ->join('candidate_informations', 'candidate_informations.id', 'finance.candidate_id')
-            ->join('candidate_educations', 'candidate_educations.candidate_id', 'finance.candidate_id')
-            ->join('candidate_positions', 'candidate_positions.candidate_id', 'finance.candidate_id')
-            ->whereIn('endorsements.remarks_for_finance', $arr)
-            ->Where('endorsements.client', 'like', '"%' . $request->searchKeyword . '%"')
-            // ->orWhere('candidate_informations.reprocess', 'like', '"%' . $request->searchKeyword . '%"')
-            ->select('endorsements.*', 'finance.*', 'finance_detail.*');
-        //  $data = DB::select('select * from six_table_view where first_name like"%'.$request->searchKeyword.'%"');
-        $sql = Str::replaceArray('?', $Userdata->getBindings(), $Userdata->toSql());
-        //start modify  wherein()
-        foreach ($arr as $remarks) {
-            $sql = str_replace($remarks, "'$remarks'", $sql);
+        $check = $searchCheck = false;
+        if ($request->searchKeyword == null) {
+            return $this->summaryAppend();
         }
-  
-        // end modify  wherein()
+        $arr = ['"Onboarded"', '"Offer Accepted"', '"Fallout"'];
+        $Userdata = DB::table('six_table_view')
+            ->whereIn('six_table_view.remarks_for_finance', $arr)
+            ->select('six_table_view.*');
+        //start search
+        if (isset($request->searchKeyword)) {
+
+            $perfect_match = DB::table("six_table_view")->get();
+
+            foreach ($perfect_match as $match) {
+                if (strpos(strtolower($match->domain), strtolower($request->searchKeyword)) !== false) {
+                    $check = true;
+                    $Userdata->where('six_table_view.domain', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->last_name), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.last_name', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->saved_by), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.saved_by', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->client), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.client', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->remarks_for_finance), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.remarks_for_finance', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->address), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.address', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->career_endo), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.career_endo', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->remarks), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.remarks', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->onboardnig_date), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.onboardnig_date', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->date_shifted), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.date_shifted', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->endi_date), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;                    $Userdata->where('six_table_view.endi_date', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->gender), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.gender', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->candidate_profile), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.candidate_profile', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->educational_attain), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.educational_attain', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->status), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.endostatus', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->first_name), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.first_name', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->last_name), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.last_name', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->curr_salary), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.curr_salary', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->placement_fee), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.placement_fee', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+                if (strpos(strtolower($match->srp), strtolower($request->searchKeyword)) !== false) {
+                     $check = true;
+                    $Userdata->where('six_table_view.srp', 'like', '"%' . $request->searchKeyword . '%"');
+                }
+            }
+        }
+         if ($check) {
+            $sql = Str::replaceArray('?', $Userdata->getBindings(), $Userdata->toSql());
+        } else {
+            if (!$check && !$searchCheck) {
+                $data = [
+                    'hires' => 0,
+                    'fallout' => 0,
+                    'billed' => 0,
+                    'unbilled' => 0,
+                    'billedAmount' => 0,
+                    'unbilledAmount' => 0,
+                    'falloutAmount' => 0,
+                    'receivablesAmount' => 0,
+                    'Current_receivablesAmount' => 0,
+                    'overDue_receivablesAmount' => 0,
+                    'ctakeAmount' => 0,
+                    'sql_c_share' => 0,
+                    'vcc_amount_sum' => 0,
+        
+                ];
+        
+                return view('finance.summary', $data);
+            } 
+        }
+       
 
         if (strpos($sql, 'where') !== false) {
-            $sql_fallout = $sql . "  and endorsements.remarks LIKE '%fallout%' OR endorsements.remarks LIKE '%replacement%'  ";
-            $sql_billed = $sql . " and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%'  ";
-            $sql_unBilled = $sql . " and endorsements.remarks ='Unbilled' ";
+            $sql_fallout = $sql . "  and six_table_view.remarks LIKE '%fallout%' OR six_table_view.remarks LIKE '%replacement%' group by six_table_view.remarks  ";
+            $sql_billed = $sql . " and six_table_view.remarks LIKE '%collect%' OR six_table_view.remarks LIKE '%replace%'OR six_table_view.remarks LIKE 'billed%' group by six_table_view.remarks  ";
+            $sql_unBilled = $sql . " and six_table_view.remarks ='Unbilled' ";
             $sql_billed_amount = DB::select($sql_billed);
             $sql_unbilled_amount = DB::select($sql_unBilled);
             $sql_fallout_amount = DB::select($sql_fallout);
-            $sql_receivables = $sql . " and finance_detail.process_status in('OVERDUE','FFUP','RCVD') ";
-            $sql_Current_receivables = $sql . " and finance_detail.process_status in('FFUP','RCVD')  ";
-            $sql_overDue_receivables = $sql . " and finance_detail.process_status ='OVERDUE' ";
-            $sql_billed = $sql . "  and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' ";
-            $c_share = $sql . "  and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' ";
-            // $sql_unbilled = $sql . "  and endorsements.remarks='Unbilled'";
-            $vcc_amount_sum = $sql . " and endorsements.remarks LIKE '%collect%' OR endorsements.remarks LIKE '%replace%'OR endorsements.remarks LIKE 'billed%' ";
-            // $sql_onboarded = $sql . " and endorsements.remarks_for_finance='Onboarded'";
+            $sql_receivables = $sql . " and six_table_view.process_status in('OVERDUE','FFUP','RCVD') ";
+            $sql_Current_receivables = $sql . " and six_table_view.process_status in('FFUP','RCVD')  ";
+            $sql_overDue_receivables = $sql . " and six_table_view.process_status ='OVERDUE' ";
+            // $sql_billed = $sql . "  and six_table_view.remarks LIKE '%collect%' OR six_table_view.remarks LIKE '%replace%'OR six_table_view.remarks LIKE 'billed%' ";
+            $c_share = $sql . "  and six_table_view.remarks LIKE '%collect%' OR six_table_view.remarks LIKE '%replace%'OR six_table_view.remarks LIKE 'billed%' group by six_table_view.remarks ";
+            $vcc_amount_sum = $sql . " and six_table_view.remarks LIKE '%collect%' OR six_table_view.remarks LIKE '%replace%'OR six_table_view.remarks LIKE 'billed%' group by six_table_view.remarks ";
         };
-
-        // $sql_receivables_amount = DB::select($sql_receivables);
-        // $sql_Current_receivables_amount = DB::select($sql_Current_receivables);
-        // $sql_overDue_receivables_amount = DB::select($sql_overDue_receivables);
+        // return $sql_billed;
+        $sql_receivables_amount = DB::select($sql_receivables);
+        $sql_Current_receivables_amount = DB::select($sql_Current_receivables);
+        $sql_overDue_receivables_amount = DB::select($sql_overDue_receivables);
         $sql_ctake_amount = DB::select($sql_billed);
         $sql_c_share_db = DB::select($c_share);
         $sql_vcc_amount_sum = DB::select($vcc_amount_sum);
@@ -439,28 +529,28 @@ class FinanceController extends Controller
         foreach ($sql_fallout_amount as $fallout) {
             $falloutAmount = $falloutAmount + $fallout->Total_bilable_ammount;
         }
-        // foreach ($sql_receivables_amount as $receivable) {
-        //     $receivablesAmount = $receivablesAmount + isset($receivable->totalFee)?$receivable->totalFee:0;
-        // }
-        // foreach ($sql_Current_receivables_amount as $Curr_receivable) {
-        //     $Current_receivablesAmount = $Current_receivablesAmount + $Curr_receivable->totalFee;
-        // }
-        // foreach ($sql_overDue_receivables_amount as $over_receivable) {
-        //     $overDue_receivablesAmount = $overDue_receivablesAmount + $over_receivable->totalFee;
-        // }
+        foreach ($sql_receivables_amount as $receivable) {
+            $receivablesAmount = $receivablesAmount + isset($receivable->totalFee) ? $receivable->totalFee : 0;
+        }
+        foreach ($sql_Current_receivables_amount as $Curr_receivable) {
+            $Current_receivablesAmount = $Current_receivablesAmount + isset($Curr_receivable->totalFee) ? $Curr_receivable->totalFee : 0;
+        }
+        foreach ($sql_overDue_receivables_amount as $over_receivable) {
+            $overDue_receivablesAmount = $overDue_receivablesAmount +  isset($over_receivable->totalFee) ? $over_receivable->totalFee : 0;
+        }
         foreach ($sql_ctake_amount as $ctake) {
             //unknown vlaue for some
-            $ctakeAmount = $ctakeAmount + $ctake->c_take_per;
+            $ctakeAmount = $ctakeAmount + $ctake->c_take;
         }
         foreach ($sql_c_share_db as $cftake) {
             //unknown vlaue for some
-            $sql_c_share = $sql_c_share + $cftake->c_take_per;
+            $sql_c_share = $sql_c_share + $cftake->c_take;
         }
         foreach ($sql_vcc_amount_sum as $cftake) {
             //unknown vlaue for some
-            $vcc_amount_sum = $vcc_amount_sum + $cftake->c_take_per;
+            $vcc_amount_sum = $vcc_amount_sum + $cftake->c_take;
         }
-     
+
         $data = [
             'hires' => count(DB::select($sql)),
             'fallout' => count(DB::Select($sql_fallout)),
