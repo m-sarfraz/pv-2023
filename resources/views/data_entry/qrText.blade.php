1193585188381d24ce937903bbc569ca98082fe3
sarfaraz-dev <muhammadsarfraz531@gmail.com>
2021-12-17 10:45:38 +0500

    qr code

--- resources/views/data_entry/qrText.blade.php
+++ resources/views/data_entry/qrText.blade.php
@@ -1,16 +1,16 @@
 Name: {{$user->last_name}};
 Email: {{$user->email}};
-Gender :{{$user->gender}};
-DOB :{{$user->dob}};
-Domain :{{$user->domain}};
-Segment :{{$user->segment}};
-Sub Segment :{{$user->sub_segment}};
-Candidate Profile :{{$user->candidate_profile}};
-Date Sifted : {{$user->date_shifted}} 
-Position Apllied :{{$user->position_applied}};
-date invited :{{$user->date_invited}};
-Manner of invite  :{{$user->manner_of_invite}};
-Source :{{$user->source}};
-current salrey :{{$user->curr_salary}};
-Expected salrey :{{$user->exp_salary}};
-Employe History :{{$user->emp_history}};
+Gender: {{$user->gender}};
+DOB: {{$user->dob}};
+Domain: {{$user->domain}};
+Segment: {{$user->segment}};
+Sub Segment: {{$user->sub_segment}};
+Candidate Profile: {{$user->candidate_profile}};
+Date Sifted: {{$user->date_shifted}} 
+Position Apllied: {{$user->position_applied}};
+date invited: {{$user->date_invited}};
+Manner of invite: {{$user->manner_of_invite}};
+Source: {{$user->source}};
+current salrey: {{$user->curr_salary}};
+Expected salrey: {{$user->exp_salary}};
+Employe History: {{$user->emp_history}};
