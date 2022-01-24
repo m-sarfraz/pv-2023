f5bb76d29d4f18085bb5a9b715e2f321b3358ecb
CDbee04 <cdbee04@thunderbees.com>
2022-01-24 16:12:09 +0500

    QRDOne

--- resources/views/data_entry/qrText.blade.php
+++ resources/views/data_entry/qrText.blade.php
@@ -1,7 +1,15 @@
-Name: @php echo   isset($user->last_name)?  $user->last_name :'N/A';@endphp
-Candidate Profile: @php echo isset( $user->candidate_profile )?  $user->candidate_profile :'N/A'@endphp;
-Position Apllied: @php echo isset( $user->position_applied )?  $user->position_applied :'N/A'@endphp;
-Manner of invite: @php echo isset( $user->manner_of_invite )?  $user->manner_of_invite :'N/A'@endphp;
-Source: @php echo isset($user->source)?  $user->source :'N/A' ;@endphp;
-Employe History: @php echo isset( $user->emp_history )?  $user->emp_history :'N/A'@endphp;
-App status: @php echo isset( $user->app_status )?  $user->app_status :'N/A'@endphp;
+Name: <?php echo   isset($user->last_name)?$user->last_name:'N/A';?>;
+Candidate Profile: <?php echo isset( $user->candidate_profile )?  $user->candidate_profile:'N/A';?>;
+Position Apllied: <?php echo isset( $user->position_applied )?$user->position_applied:'N/A';?>;
+Manner of invite: <?php echo isset( $user->manner_of_invite )?$user->manner_of_invite:'N/A';?>;
+Source: <?php echo isset($user->source)?$user->source :'N/A';?>;
+Employe History: 
+<?php 
+ if(isset($user->emp_history))
+ {
+    echo implode(' ', array_slice(explode(' ', $user->emp_history), 0, 20))."\n";
+ }else{
+    echo "N/A";
+ }
+?>;
+App status: <?php echo isset( $user->app_status )?$user->app_status:'N/A';?>;
